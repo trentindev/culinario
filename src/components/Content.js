852fa5104788d2167b4d150d68fcf2f3bfd42fd9
Laurent Dislaire <siladire@free.fr>
2023-06-23 14:34:53 +0200

    Responsive style and mixins

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,22 +1,18 @@
 import styles from './Content.module.scss';
 import Recipe from './Recipe';
+import { data } from '../data/recipes';
 
 function Content() {
+  const recipes = data;
+
   return (
     <div className="flex-fill container p-20">
       <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
       <div className={`card p-20 ${styles.contentCard}`}>
         <div className={styles.grid}>
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
-          <Recipe />
+          {recipes.map((r) => (
+            <Recipe key={r._id} title={r.title} image={r.image} />
+          ))}
         </div>
       </div>
     </div>
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,16 +1,23 @@
-import styles from "./Recipe.module.scss";
-import recipe from "../assets/images/recette.jpg";
+import { useState } from 'react';
+import styles from './Recipe.module.scss';
 
-function Recipe() {
+function Recipe({ title, image }) {
+  const [liked, setLiked] = useState(false);
+
+  function handleClick() {
+    setLiked(!liked);
+  }
+
   return (
-    <div className={styles.recipe}>
+    <div onClick={handleClick} className={styles.recipe}>
       <div className={styles.imageContainer}>
-        <img src={recipe} alt="recipe" />
+        <img src={image} alt={title} />
       </div>
       <div
-        className={`${styles.recipeTitle} d-flex flex-row justify-content-center align-items-center`}
+        className={`${styles.recipeTitle} d-flex flex-column justify-content-center align-items-center`}
       >
-        <h3>Tarte au comcombres</h3>
+        <h3 className="mb-10">{title}</h3>
+        <i className={`fa-solid fa-heart ${liked ? 'text-primary' : ''}`}></i>
       </div>
     </div>
   );
