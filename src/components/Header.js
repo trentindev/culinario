3cdffca694b767c2e0638bf306c31e6b34e10ef1
Laurent Dislaire <siladire@free.fr>
2023-06-23 14:48:17 +0200

    Header and Menu modification

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,33 @@
 import styles from "./Header.module.scss";
-import culinario from "../assets/images/culinario.png";
+import cookchef from "../assets/images/culinario.png";
+import { useState } from "react";
+import HeaderMenu from "./HeaderMenu";
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
+
   return (
     <header className={`${styles.header} d-flex flex-row align-items-center`}>
-      <i className="fa-solid fa-bars mr-15"></i>
       <div className="flex-fill">
-        <img src={culinario} alt="logo culinario" />
+        <img src={cookchef} alt="logo cookchef" />
       </div>
-      <ul>
+      <ul className={styles.headerList}>
         <button className="mr-5 btn btn-reverse-primary">
-          <i className="fa-solid fa-basket-shopping mr-5"></i>
-          <span>panier</span>
+          <i className="fa-solid fa-heart mr-5"></i>
+          <span>Wishlist</span>
         </button>
-        <button className="btn btn-primary">connexion</button>
+        <button className="btn btn-primary">Connexion</button>
       </ul>
+      <i
+        onClick={() => setShowMenu(true)}
+        className={`fa-solid fa-bars ${styles.headerXs}`}
+      ></i>
+      {showMenu && (
+        <>
+          <div onClick={() => setShowMenu(false)} className="calc"></div>
+          <HeaderMenu />
+        </>
+      )}
     </header>
   );
 }
