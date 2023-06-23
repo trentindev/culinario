6028abbfc238de8e1672800ab1505729140eed62
Laurent Dislaire <siladire@free.fr>
2023-06-23 13:44:20 +0200

    Content Layout

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,26 @@
+import styles from './Content.module.scss';
+import Recipe from './Recipe';
+
 function Content() {
-  return <h1 className="flex-fill b3">Content</h1>;
+  return (
+    <div className="flex-fill container p-20">
+      <h1 className="my-30">Découvrez nos nouvelles recettes</h1>
+      <div className={`card p-20 ${styles.contentCard}`}>
+        <div className={styles.grid}>
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Content;
